5b2585d089b2c51c91a4aa31eb4d5ce98fe33c0f
Aoi Kondo <agotadmidori@gmail.com>
2021-09-23 09:02:26 +0900

    AC

---
+++ AtCoder/ABC/ABC205/C.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <complex.h>
+#include <assert.h>
+#include <string.h>
+#include <ctype.h>
+
+#define DBG_INT(arg) fprintf(stderr,"*DEBUG* " #arg "...%d\n",arg)
+#define DBG_DOUBLE(arg) fprintf(stderr,"*DEBUG* " #arg "...%f\n",arg)
+#define DBG_CHAR(arg) fprintf(stderr,"*DEBUG* " #arg "...%c\n",arg)
+#define DBG_STRING(arg) fprintf(stderr,"*DEBUG* " #arg "...%s\n",arg)
+#define mal(type,size)  malloc(size*sizeof(type))
+#define rep(i,N)    for(int i=0;i<N;i++)
+
+typedef long long ll;
+
+signed main(void){
+  int A,B,C;
+  scanf("%d %d %d",&A,&B,&C);
+  if(C%2==0){
+      A=abs(A);
+      B=abs(B);
+      C=abs(C);
+    }
+  if(A==B){
+      puts("=");
+    }
+  else if(A>B){
+      puts(">");
+    }
+  else if(A<B){
+      puts("<");
+    }
+    return 0;
+}
