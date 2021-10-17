939b527c478e019b619412d998c335dd06a40578
Aoi Kondo <agotadmidori@gmail.com>
2021-10-17 09:43:46 +0900

    AC

---
+++ AtCoder/ABC/Series3(41-60)/ABC056/B.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <complex.h>
+#include <assert.h>
+#include <string.h>
+#include <ctype.h>
+
+#define DBG_INT(arg) fprintf(stderr,"*DEBUG* " #arg "...%d\n",arg)
+#define DBG_DOUBLE(arg) fprintf(stderr,"*DEBUG* " #arg "...%f\n",arg)
+#define DBG_CHAR(arg) fprintf(stderr,"*DEBUG* " #arg "...%c\n",arg)
+#define DBG_STRING(arg) fprintf(stderr,"*DEBUG* " #arg "...%s\n",arg)
+#define mal(type,size)  malloc(size*sizeof(type))
+#define rep(i,N)    for(int i=0;i<N;i++)
+#define rep2(i,N,M) for(int i=N;i<M;i++)
+
+typedef long long ll;
+
+signed main(void){
+    int W,a,b;
+    scanf("%d %d %d",&W,&a,&b);
+    int ans=b-(a+W);
+    if(a+W>b){
+        ans=0;
+    }
+    if(a>b){
+        ans=a-(b+W);
+    }
+    printf("%d\n",ans);
+}
