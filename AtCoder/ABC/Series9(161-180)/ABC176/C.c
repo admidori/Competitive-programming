5dbf831f277d928e584decec1dea72cbf00468b1
Aoi Kondo <agotadmidori@gmail.com>
2021-10-20 17:47:34 +0900

    AC

---
+++ AtCoder/ABC/Series9(161-180)/ABC176/C.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <complex.h>
+#include <assert.h>
+#include <string.h>
+#include <ctype.h>
+
+#define DBG_INT(arg) fprintf(stderr,"*DEBUG* " #arg "...%d\n",arg)
+#define DBG_DOUBLE(arg) fprintf(stderr,"*DEBUG* " #arg "...%f\n",arg)
+#define DBG_CHAR(arg) fprintf(stderr,"*DEBUG* " #arg "...%c\n",arg)
+#define DBG_STRING(arg) fprintf(stderr,"*DEBUG* " #arg "...%s\n",arg)
+#define mal(type,size)  malloc(size*sizeof(type))
+#define rep(i,N)    for(int i=0;i<N;i++)
+#define rep2(i,N,M) for(int i=N;i<M;i++)
+
+typedef long long ll;
+
+char *string_scan(char *S){
+    S=mal(char,10);
+    int i=0;
+    while(1){
+        if(i==9){
+            S=realloc(S,10);
+        }
+        scanf("%c",&S[i]);
+        if(S[i]=='\0'){
+            break;
+        }
+        i++;
+    }
+    return S;
+}
+
+signed main(void){
+    long long int N;
+    scanf("%llu",&N);
+    int *A;
+    A=mal(int,N);
+    rep(i,N){
+        scanf("%d",&A[i]);
+    }
+
+    long long int cnt=0;
+    rep2(i,1,N){
+        if(A[i-1]>=A[i]){
+            cnt+=A[i-1]-A[i];
+            A[i]+=A[i-1]-A[i];
+        }
+    }
+    printf("%llu\n",cnt);
+    return 0;
+}
