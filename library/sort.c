abf4c35398f60406955af759d719be64992b2795
Aoi Kondo <agotadmidori@gmail.com>
2022-06-11 13:23:05 +0900

    swap add

---
+++ library/sort.c
@@ -0,0 +1,30 @@
+void swap(int *a,int *b){
+    int tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+/*
+    Sellection Sort
+    O(N^2)
+*/
+void Sellection_sort(int N,int *array){
+    
+}
+
+void Bubble_sort(int N,int *array){
+
+}
+
+void Insertion_sort(int N,int *array){
+
+}
+
+/*
+    Quick Sort
+    O(NlogN)
+*/
+void Quick_sort(int N,int *array){
+
+}
